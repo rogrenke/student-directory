0af08f9a037070096953242f4b12ac3bfa3e3fd2
“rogrenke” <roland@rolandgrenke.com>
2017-04-28 17:32:28 +0200

    step 8, ex 5, added info on hobbies, country of birth, height

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: "Being evil", country_of_birth: "World", height: "2.00m" }
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -19,7 +19,7 @@ end
 def print(students)
   idx = 0
   while idx < students.length
-    puts "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort) "
+    puts "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort, hobbies: #{students[idx][:hobbies]}, country of birth: #{students[idx][:country_of_birth]}, height: #{students[idx][:height]}) "
     idx += 1
   end
 end
@@ -30,7 +30,7 @@ def print_by_letter(students)
   puts "Those are the students starting with '#{input}'"
   students.each_with_index do |student, idx|
     if input[0].downcase == student[:name][0].downcase
-      puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort, hobbies: #{student[:hobbies]}, country of birth: #{student[:country_of_birth]}, height: #{student[:height]})"
     end
   end
 end
@@ -39,7 +39,7 @@ def print_12_chars(students)
   puts "Those are the students whose name is 12 characters long or shorter"
   students.each_with_index do |student, idx|
     if student[:name].length <= 12
-      puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{idx+1}. #{student[:name]} (#{student[:cohort]} cohort, hobbies: #{student[:hobbies]}, country of birth: #{student[:country_of_birth]}, height: #{student[:height]})"
     end
   end
 end
